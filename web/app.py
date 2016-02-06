4b944459433ce7adbd669fb160e8ea357cfed08d
vagrant <vagrant@precise32.(none)>
2016-02-06 00:30:50 +0000

    Initial commit of web stuff

---
+++ web/app.py
@@ -0,0 +1,30 @@
+'''
+app.py
+'''
+
+import urllib
+from markupsafe import Markup
+from flask import Flask, render_template, request, redirect
+from flask.ext.mongoengine import MongoEngine
+from flask.ext.mongoengine.wtf import model_form
+from wtforms import PasswordField
+from flask.ext.login import LoginManager, login_user, logout_user, login_required, current_user
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def search():
+	return render_template("search.html")
+
+
+'''
+@app.after_request
+def add_header(response):
+	response.cache_control.max_age = 5
+	return response
+'''
+		
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
+	
